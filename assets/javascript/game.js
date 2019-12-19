60b0fc5a96025ce39c9431dbf3cdcecb422a2f3e
Mangenese <blakemangen20@gmail.com>
2019-12-18 23:06:29 -0700

    added some css, changed the background, pushed it to the center, fixed some bugs on the html, and tweaked a few settings in js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,36 +20,173 @@ document.getElementById("guess-remaining").innerHTML = guessesLeft;
 
 
 // Computer displays a random word as a "_",
+var getNewWord =function () {
+    var randomWord = words[Math.floor(Math.random() * words.length)].toLowerCase();
+    console.log(randomWord);
+    for (var i = 0; i < randomWord.length; i++){
+        blankSpace[i] = "_" ;
+    
+    }
+    
+        document.getElementById("play-area").innerHTML = blankSpace;
+     // Computer displays an image to the img div
+        if (randomWord === words[0].toLowerCase()) {
+    
+            document.getElementById("img").innerHTML = "<img src = 'assets/images/Ephraim.png'>"
+        } 
+        else  if (randomWord === words[1].toLowerCase()) {
+            
+            document.getElementById("img").innerHTML = "<img src = 'assets/images/Chrom.png'>"
+        } 
+        else  if (randomWord === words[2].toLowerCase()) {
+            
+            document.getElementById("img").innerHTML = "<img src = 'assets/images/Corrin.png'>"
+        } 
+        else  if (randomWord === words[3].toLowerCase()) {
+            
+            document.getElementById("img").innerHTML = "<img src = 'assets/images/Ike.png'>"
+        } 
+        else  if (randomWord === words[4].toLowerCase()) {
+            
+            document.getElementById("img").innerHTML = "<img src = 'assets/images/Lucina.png'>"
+        } 
+        else  if (randomWord === words[5].toLowerCase()) {
+        
+            document.getElementById("img").innerHTML = "<img src = 'assets/images/Marth.png'>"
+        } 
+        else  if (randomWord === words[6].toLowerCase()) {
+        
+            document.getElementById("img").innerHTML = "<img src = 'assets/images/Robin.png'>"
+        }
+         else  if (randomWord === words[7].toLowerCase()) {
+        
+            document.getElementById("img").innerHTML = "<img src = 'assets/images/Alm.png'>"
+        }
+         else  if (randomWord === words[8].toLowerCase()) {
+        
+            document.getElementById("img").innerHTML = "<img src = 'assets/images/Celica.png'>"
+        }
+         else  if (randomWord === words[9].toLowerCase()) {
+        
+            document.getElementById("img").innerHTML = "<img src = 'assets/images/Byleth.png'>"
+        }
+         else  if (randomWord === words[10].toLowerCase()) {
+        
+            document.getElementById("img").innerHTML = "<img src = 'assets/images/Roy.png'>"
+        }
+         else  if (randomWord === words[11].toLowerCase()) {
+        
+            document.getElementById("img").innerHTML = "<img src = 'assets/images/Eirika.png'>"
+        }
+         else  if (randomWord === words[12].toLowerCase()) {
+        
+            document.getElementById("img").innerHTML = "<img src = 'assets/images/Lyn.png'>"
+        } 
+        document.onkeyup = function (event) {
+            var userGuess = event.key;
+            console.log(userGuess)
+        
+            var isGuessWrong = true;
+        
+            for (var i= 0; i < randomWord.length; i++) {
+                if (userGuess === randomWord[i]) {
+                    wordLength--;
+                    console.log(wordLength);
+                    isGuessWrong = false;
+                    blankSpace[i] = userGuess;
+                    document.getElementById("play-area").innerHTML = " " + blankSpace.join(" ");
+                }
+            }
+            if (isGuessWrong === true) {
+                if (guessesLeft > 0 ) {
+                    letterUsed.push(userGuess);
+                    document.getElementById("letters-guessed").innerHTML = " " + letterUsed.join(" ");
+                    guessesLeft--;
+                    document.getElementById("guess-remaining").innerHTML = guessesLeft;
+                }
+                if (guessesLeft === 0) {
+                    loss++;
+                    document.getElementById("loss").innerHTML = loss;
+                    alert("You lost! Some kind of tactician you are! Click the screen to try again!");
+                    randomWord= getNewWord();
+                    // guessesLeft = 12;
+                }
+               
+            }
+        
+            if (wordLength === 0) {
+                win++;
+                document.getElementById("wins").innerHTML = win;
+                alert("You win! You are quite the tactician! Click the screen to try again");
+                randomWord = getNewWord();
+                // guessesLeft = 12;
+        
+            }
+        
+        }
+        
+    
+    return randomWord;
+    // return guessesLeft= 12
+}
+
 for (var i = 0; i < randomWord.length; i++){
     blankSpace[i] = "_" ;
 
-    document.getElementById("play-area").innerHTML = blankSpace;
+}
 
+    document.getElementById("play-area").innerHTML = blankSpace;
+ // Computer displays an image to the img div
     if (randomWord === words[0].toLowerCase()) {
+
         document.getElementById("img").innerHTML = "<img src = 'assets/images/Ephraim.png'>"
-    } else  if (randomWord === words[1].toLowerCase()) {
+    } 
+    else  if (randomWord === words[1].toLowerCase()) {
+        
         document.getElementById("img").innerHTML = "<img src = 'assets/images/Chrom.png'>"
-    } else  if (randomWord === words[2].toLowerCase()) {
+    } 
+    else  if (randomWord === words[2].toLowerCase()) {
+        
         document.getElementById("img").innerHTML = "<img src = 'assets/images/Corrin.png'>"
-    } else  if (randomWord === words[3].toLowerCase()) {
+    } 
+    else  if (randomWord === words[3].toLowerCase()) {
+        
         document.getElementById("img").innerHTML = "<img src = 'assets/images/Ike.png'>"
-    } else  if (randomWord === words[4].toLowerCase()) {
+    } 
+    else  if (randomWord === words[4].toLowerCase()) {
+        
         document.getElementById("img").innerHTML = "<img src = 'assets/images/Lucina.png'>"
-    } else  if (randomWord === words[5].toLowerCase()) {
+    } 
+    else  if (randomWord === words[5].toLowerCase()) {
+    
         document.getElementById("img").innerHTML = "<img src = 'assets/images/Marth.png'>"
-    } else  if (randomWord === words[6].toLowerCase()) {
+    } 
+    else  if (randomWord === words[6].toLowerCase()) {
+    
         document.getElementById("img").innerHTML = "<img src = 'assets/images/Robin.png'>"
-    } else  if (randomWord === words[7].toLowerCase()) {
+    }
+     else  if (randomWord === words[7].toLowerCase()) {
+    
         document.getElementById("img").innerHTML = "<img src = 'assets/images/Alm.png'>"
-    } else  if (randomWord === words[8].toLowerCase()) {
+    }
+     else  if (randomWord === words[8].toLowerCase()) {
+    
         document.getElementById("img").innerHTML = "<img src = 'assets/images/Celica.png'>"
-    } else  if (randomWord === words[9].toLowerCase()) {
+    }
+     else  if (randomWord === words[9].toLowerCase()) {
+    
         document.getElementById("img").innerHTML = "<img src = 'assets/images/Byleth.png'>"
-    } else  if (randomWord === words[10].toLowerCase()) {
+    }
+     else  if (randomWord === words[10].toLowerCase()) {
+    
         document.getElementById("img").innerHTML = "<img src = 'assets/images/Roy.png'>"
-    } else  if (randomWord === words[11].toLowerCase()) {
+    }
+     else  if (randomWord === words[11].toLowerCase()) {
+    
         document.getElementById("img").innerHTML = "<img src = 'assets/images/Eirika.png'>"
-    } else  if (randomWord === words[12].toLowerCase()) {
+    }
+     else  if (randomWord === words[12].toLowerCase()) {
+    
         document.getElementById("img").innerHTML = "<img src = 'assets/images/Lyn.png'>"
     } 
     
@@ -82,6 +219,8 @@ document.onkeyup = function (event) {
         if (guessesLeft === 0) {
             loss++;
             document.getElementById("loss").innerHTML = loss;
+            alert("You lost! Some kind of tactician you are! Click the screen to try again!");
+            randomWord= getNewWord();
         }
        
     }
@@ -89,8 +228,10 @@ document.onkeyup = function (event) {
     if (wordLength === 0) {
         win++;
         document.getElementById("wins").innerHTML = win;
+        alert("You win! You are quite the tactician! Click the screen to try again");
+        randomWord = getNewWord();
+
     }
 
 }
 
-}
